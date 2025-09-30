bf8c6de296364e82d790964dcf0dbb5da6d0c286
Nafi Hanif <naafiik1@gmail.com>
2025-09-30 10:49:11 +0700

    just trial

--- week7/Hello3.java
+++ week7/Hello3.java
@@ -2,7 +2,7 @@ package week7;
 
 public class Hello3 {
     public static void main(String[] args) {
-        System.out.println("halo sam bagaimana kabarmu ");
+        System.out.println("halo sam bagaimana kabarmu  ");
     }
     
 }
