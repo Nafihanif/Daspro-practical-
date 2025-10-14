f0cc9da2da006bdc934273b4f3895a26977214b6
Nafi Hanif <naafiik1@gmail.com>
2025-10-14 07:00:25 +0700

    modification 1 jobsheet 5

---
+++ jobSheet5/ifPrintKRSNafi.java
@@ -0,0 +1,16 @@
+package jobSheet5;
+import java.util.Scanner;
+public class ifPrintKRSNafi {
+    public static void main(String[] args) {
+        Scanner twk = new Scanner (System.in);
+
+        System.out.println("\n---Print KRS SIAKAD---");
+        System.out.print("Has the UKT been paid in full? (true/false)");
+        boolean uktpaid = twk.nextBoolean();
+
+        if (uktpaid){
+            System.out.println("verified UKT payment");
+            System.out.println("Please print the KRS and ask for the DPA's signature");
+        }
+    }
+}
