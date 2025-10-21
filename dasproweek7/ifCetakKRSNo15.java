5df59f693ded204885b12b3aa6c7af47e1dcc901
Nafi Hanif <naafiik1@gmail.com>
2025-10-21 20:21:56 +0700

    task 1 first step

---
+++ dasproweek7/ifCetakKRSNo15.java
@@ -0,0 +1,12 @@
+package dasproweek7;
+
+public class ifCetakKRSNo15 {
+    public static void main(String[] args) {
+        boolean presence = true;
+        if (presence) {
+            System.out.println("KRS printing successful");
+        } else {
+            System.out.println("Cannot print KRS");
+        }
+    }
+}
