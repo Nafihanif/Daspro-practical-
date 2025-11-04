ffbef1122df194cfa272d07f4ede4a9889626e14
Nafi Hanif <naafiik1@gmail.com>
2025-11-04 09:16:58 +0700

    array avarage score 2

--- jobsheet9/arrayAvarageScore.java
+++ jobsheet9/arrayAvarageScore.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class arrayAvarageScore {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int [] score =new int [10];
+        System.out.print("Enter the muber of student : ");
+        int c = sc.nextInt();
+
+        int [] score =new int [c];
         double total = 0;
         double avarage ;
 
@@ -13,9 +16,18 @@ public class arrayAvarageScore {
         } 
         for (int i = 0; i < score.length; i++) {
             total += score[i];
+
+            if (score[i] > 70){
+                System.out.println("Student " + (i+1) +"score :" + score[i]);
+                System.out.println("pass");
+        }else{
+            System.out.println("Student " + (i+1) + "score :" + score[i]);
+            System.out.println("failed");
         }
+    }
+
         avarage = total/score.length;
         System.out.println("the class avarage score is : " + avarage );
-    }
-    
+sc.close();
+}
 }
